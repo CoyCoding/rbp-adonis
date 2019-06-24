31aa576ac45819e5908775934ccc9489dc977d78
Matt Coy <fluffytoycoy@gmail.com>
2019-06-24 12:23:20 -0500

    switched to jwt to fix react auth problem

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -7,12 +7,16 @@ const Token = use('App/Models/Token');
 class AdminController {
   async index({response, request, auth, session}){
     const {username, password} = request.all();
-    // console.log(response)
     try{
-         console.log('logged in')
-         await auth.attempt(username, password)
-         // response.cookie.('session', auth._ctx.session._sessionId);
-
+         console.log('logged in');
+         await auth.attempt(username, password);
+         let user = await User.findBy('username', username)
+         console.log(user)
+         let accessToken = await auth.generate(user)
+         return response.json({"user":user, "access_token": accessToken})
+         // console.log(user)
+         // const accessToken = await auth.generate(user);
+         // return response.json({user: user,access_token: accessToken})
     }catch (e){
       console.log(e)
       return e
@@ -21,8 +25,6 @@ class AdminController {
   async logout({respnose, request, auth, session}){
     console.log('logged off');
     try{
-      //console.log(respnose.cookie('session'))
-        console.log(request.cookie('session'))
         await auth.logout();
     }catch(e){
       console.log(e)
--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -26,7 +26,6 @@ export default class App extends Component {
 	};
 
 	render() {
-		console.log(cookies)
 		return (
 
       <Router>
